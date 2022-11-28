1b6e2c1479822d1ad41623a00de51973a5e42d43
devofficer <myeyesontech@gmail.com>
2022-11-28 13:13:37 -0800

    [fix] updated image type

--- containers/Marketplace/components/Header.tsx
+++ containers/Marketplace/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <nav className="flex justify-between w-full bg-slate-900 sticky top-0 z-40 lg:border-b border-solid border-black/75">
       <div className="flex justify-between items-center pt-4 sm:pt-0 pb-4 sm:pb-0">
-      <img src="logo-x.png" alt="logo" className="flex items-center sm:ml-5" style={{width: '30px'}} />
+        <img src="logo-x.png" alt="logo" className="flex items-center sm:ml-5" style={{width: '30px'}} />
         <h1 className="font-light text-2xl text-white p-2 sm:p-4 border-gray-100 hidden sm:block">
           SpaceRanch DAO
         </h1>
