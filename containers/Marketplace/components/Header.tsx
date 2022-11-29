88adb09ed4d3c7cd30720f6ff3377e877d5c5677
devofficer <myeyesontech@gmail.com>
2022-11-29 08:35:50 -0800

    [fix] removed image logo on header and footer

--- containers/Marketplace/components/Header.tsx
+++ containers/Marketplace/components/Header.tsx
@@ -7,7 +7,6 @@ const Header = () => {
   return (
     <nav className="flex justify-between w-full bg-base-blue-dark sticky top-0 z-40 lg:border-b border-solid border-black/75 px-6">
       <div className="flex justify-between items-center pt-4 sm:pt-0 pb-4 sm:pb-0">
-        <img src="logo-x.png" alt="logo" className="flex items-center" style={{ width: '30px' }} />
         <h1 className="font-light text-2xl text-white p-2 sm:p-4 border-gray-100 hidden sm:block">
           SpaceRanch DAO
         </h1>
