762d8b1eb2ca86fd67a79091459ce921c0e0f5a7
devofficer <myeyesontech@gmail.com>
2022-11-30 09:24:46 -0800

    [feat] updated the text on first page

--- containers/Marketplace/components/HeroSection.tsx
+++ containers/Marketplace/components/HeroSection.tsx
@@ -1,27 +1,38 @@
-
 const HeroSection = () => {
   return (
     <div className="flex flex-1 flex-col items-center justify-center">
       <div className="items-center justify-center">
-        {/*<MbText
-          className="text-3xl uppercase p-4 border-gray-100"
-        >
-          NFT Membership Selection
-        </MbText> <img src="logo-x.png" className="flex items-center float-left p-4" style={{width: '130px'}} /> */}
-
-        <div className="pl-4 pr-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 gap-4 my-12 text-white">
-          <div className="text-xl text-white mt-1 font-bold">
-          &#62;&#62;&#62; Live Demo on NEAR Testnet <br/><br/>
-            Join us on our mission to collaboratively build the future of space exploration
+        <div className="pl-4 pr-4 text-white">
+          <div className="text-xl text-white mt-2 font-bold">
+            Our mission is to build towards the Space Age.
           </div>
-          <div className="">
-          <span className="text-white">The SpaceRanch Membership is a mechanism for governance and applications built for the SpaceRanch ecosystem, which includes access to the physical properties and digital immersive environments.</span>
-            <span className="text-white">There are five ranks which represent different tiers of opportunities and responsibilities within the organization. Climb the ranks by completing bounties and participating in the SpaceRanch.</span> 
+          <div className="flex flex-col justify-between py-2 space-y-2 md:flex-row md:space-x-2 md:space-y-0">
+            <div className="flex-1">
+              We are innovating new ways to organize and develop infrastructure
+              for the emerging space industry. With peer to peer technologies
+              that enable alternative fundraising and transparent governance -
+              we will go far as a next generation space organization, and boldly
+              go where no group has gone before.
+            </div>
+            <div className="flex-1">
+              <div className="text-white">
+                The SpaceRanch Membership is the access point for participating
+                with the SpaceRanch ecosystem, where you can vote and engage in
+                the creation of launch infrastructure, and the proliferation of
+                new space technologies.
+              </div>
+              <div className="text-white">
+                There are five ranks which represent different tiers of
+                opportunities and responsibilities within the organization. Climb
+                the ranks by completing bounties, fulfilling roles, and building
+                towards the future.
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default HeroSection;
