9146dd81a663d23d08c7aa36ce05cbe43f8bc3cf
SpaceRanch Dev <91358513+SpaceRanchDev@users.noreply.github.com>
2022-11-30 07:15:12 -0800

    Update HeroSection.tsx
    fixed bug

--- containers/Marketplace/components/HeroSection.tsx
+++ containers/Marketplace/components/HeroSection.tsx
@@ -9,10 +9,10 @@ const HeroSection = () => {
 
         <div className="pl-4 pr-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 gap-4 my-12 text-white">
           <div className="text-xl text-white mt-1 font-bold">
-            Our mission is to collaboratively build towards the Space Age.
+            Our mission is to build towards the Space Age.</div>
           <span className="text-white">We are innovating new ways to organize and develop infrastructure for the emerging space industry. With next generation peer to peer technologies, 
             enabling alternative fundraising and trustless governance opportunities - we will go far as a next generation space organization, and boldly go where no group has gone before.</span>
-          </div>
+          
           <div className="">
           <span className="text-white">The SpaceRanch Membership is the access point for participating with the SpaceRanch ecosystem.  to the physical properties and digital immersive environments.</span>
             <span className="text-white">There are five ranks which represent different tiers of opportunities and responsibilities within the organization. Climb the ranks by completing bounties and participating in the SpaceRanch.</span> 
